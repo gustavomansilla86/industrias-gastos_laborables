b80408cea5974e0c1d6f7247cdac4580fb4c7382
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2025-05-30 02:46:53 -0300

    plantilla

---
+++ costos_laborables/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='se ralizara una previo analisis para tratar de predecir cual seria los costos laborables por sector de forma de poder predecir que impacto tendria en la provicia el desarme de un sector o posible invercion',
+    author='Mansilla Gustavo Gabriel',
+    license='MIT',
+)
